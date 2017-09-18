07ce56ca9e3e01ff6ca5eafdf6b6723540c82f5f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-18 20:24:13 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -8,12 +8,12 @@ describe('Selectors', () => {
     expect(lastImageSelector().length).toBe(1);
     expect(lastImageSelector().attr('src')).toEqual("https://s3.amazonaws.com/after-school-assets/confused.gif");
   });
-/*
+
   it('ninjaBabySelector() should return the image of the ninja baby', function() {
     expect(ninjaBabySelector().length).toBe(1);
     expect(ninjaBabySelector().attr('src')).toEqual("https://s3.amazonaws.com/after-school-assets/baby-ninja.gif");
   });
-
+/*
   it('divSelector() should return two divs', function() {
     expect(divSelector().length).toBe(2);
     expect(divSelector()[0].tagName).toEqual("DIV");
